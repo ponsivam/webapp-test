d3eaccb61341337e3934d23fbe1e6615344f2306
Sundaravadivu <m.sundaravadivu@gmail.com>
2020-09-26 18:56:01 +0100

    update

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace sivam_webapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! -Sivam");
                 });
             });
         }
